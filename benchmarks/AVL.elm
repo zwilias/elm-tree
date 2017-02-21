c4def5275f75b5b00fa5c84cc43cbffcd03fcaec
Ilias <mail@ilias.xyz>
2017-02-21 16:40:08 +0100

    Add initial benchmarks

---
+++ benchmarks/AVL.elm
@@ -0,0 +1,55 @@
+module AVL exposing (..)
+
+import Benchmark exposing (..)
+import Benchmark.Runner exposing (BenchmarkProgram, program)
+import Char
+import Dict
+import Dict.AVL as AltDict
+import Time exposing (Time)
+
+
+forValues : String -> Int -> (Int -> comparable) -> (Int -> b) -> Benchmark
+forValues name size keyer valuer =
+    let
+        source =
+            List.map
+                (\n -> ( keyer n, valuer n ))
+                (List.range 1 size)
+
+        key =
+            keyer size
+
+        value =
+            valuer size
+    in
+        describe (name ++ " (" ++ toString size ++ " items)")
+            [ Benchmark.compare "insert"
+                (Benchmark.benchmark3 "Dict.AVL" AltDict.insert key value (AltDict.fromList source))
+                (Benchmark.benchmark3 "Dict" Dict.insert key value (Dict.fromList source))
+            , Benchmark.compare "get"
+                (Benchmark.benchmark2 "Dict.AVL" AltDict.get key (AltDict.fromList source))
+                (Benchmark.benchmark2 "Dict" Dict.get key (Dict.fromList source))
+            , Benchmark.compare "remove"
+                (Benchmark.benchmark2 "Dict.AVL" AltDict.remove key (AltDict.fromList source))
+                (Benchmark.benchmark2 "Dict" Dict.remove key (Dict.fromList source))
+            , Benchmark.compare "toList"
+                (Benchmark.benchmark1 "Dict.AVL" AltDict.toList (AltDict.fromList source))
+                (Benchmark.benchmark1 "Dict" Dict.toList (Dict.fromList source))
+            ]
+
+
+suiteOfSize : Int -> Benchmark
+suiteOfSize size =
+    describe ("dicts of size " ++ toString size)
+        [ forValues "int" size identity (always ())
+        , forValues "float" size toFloat (always ())
+        , forValues "time" size (toFloat >> (*) Time.millisecond) (always ())
+        , forValues "char" size Char.fromCode (always ())
+        , forValues "string" size toString (always ())
+        , forValues "tuple of int" size (\i -> ( i, i )) (always ())
+        ]
+
+
+main : BenchmarkProgram
+main =
+    program <| describe "dicts" <| List.map suiteOfSize [ 0, 1, 10, 100, 1000, 10000 ]
---
+++ benchmarks/TwoThree.elm
@@ -0,0 +1,55 @@
+module TwoThree exposing (..)
+
+import Benchmark exposing (..)
+import Benchmark.Runner exposing (BenchmarkProgram, program)
+import Char
+import Dict
+import Dict.TwoThree as AltDict
+import Time exposing (Time)
+
+
+forValues : String -> Int -> (Int -> comparable) -> (Int -> b) -> Benchmark
+forValues name size keyer valuer =
+    let
+        source =
+            List.map
+                (\n -> ( keyer n, valuer n ))
+                (List.range 1 size)
+
+        key =
+            keyer size
+
+        value =
+            valuer size
+    in
+        describe (name ++ " (" ++ toString size ++ " items)")
+            [ Benchmark.compare "insert"
+                (Benchmark.benchmark3 "Dict.TwoThree" AltDict.insert key value (AltDict.fromList source))
+                (Benchmark.benchmark3 "Dict" Dict.insert key value (Dict.fromList source))
+            , Benchmark.compare "get"
+                (Benchmark.benchmark2 "Dict.TwoThree" AltDict.get key (AltDict.fromList source))
+                (Benchmark.benchmark2 "Dict" Dict.get key (Dict.fromList source))
+            , Benchmark.compare "remove"
+                (Benchmark.benchmark2 "Dict.TwoThree" AltDict.remove key (AltDict.fromList source))
+                (Benchmark.benchmark2 "Dict" Dict.remove key (Dict.fromList source))
+            , Benchmark.compare "toList"
+                (Benchmark.benchmark1 "Dict.TwoThree" AltDict.toList (AltDict.fromList source))
+                (Benchmark.benchmark1 "Dict" Dict.toList (Dict.fromList source))
+            ]
+
+
+suiteOfSize : Int -> Benchmark
+suiteOfSize size =
+    describe ("dicts of size " ++ toString size)
+        [ forValues "int" size identity (always ())
+        , forValues "float" size toFloat (always ())
+        , forValues "time" size (toFloat >> (*) Time.millisecond) (always ())
+        , forValues "char" size Char.fromCode (always ())
+        , forValues "string" size toString (always ())
+        , forValues "tuple of int" size (\i -> ( i, i )) (always ())
+        ]
+
+
+main : BenchmarkProgram
+main =
+    program <| describe "dicts" <| List.map suiteOfSize [ 0, 1, 10, 100, 1000, 10000 ]
